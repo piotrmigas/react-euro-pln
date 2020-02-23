25032e628785c9f8be51d217cd5d9d751e08f7c1
piotrmigas <piotr.migas@icloud.com>
2020-02-23 20:29:33 +0100

    commit

--- src/index.js
+++ src/index.js
@@ -5,10 +5,16 @@ import { Provider } from "react-redux";
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import dataReducer from "./redux/dataReducer";
+import throttle from "lodash/throttle";
 
 let initialState = {
   transactions: []
 };
+
+const persistedState = localStorage.getItem("transactions");
+if (persistedState) {
+  initialState = JSON.parse(persistedState);
+}
 
 const store = createStore(
   dataReducer,
@@ -17,6 +23,12 @@ const store = createStore(
     applyMiddleware(thunk),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
+);
+
+store.subscribe(
+  throttle(() => {
+    localStorage.setItem("transactions", JSON.stringify(store.getState()));
+  })
 );
 
 ReactDOM.render(
